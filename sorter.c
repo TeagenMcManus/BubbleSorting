01a4ece8ce4901d739310b2a58117c66ee4cc571
Teagen.A.M <88509144+TeagenMcManus@users.noreply.github.com>
2026-01-22 21:35:15 +0000

    Completed the bubble sorting. Couple of things to metion; was told not to mess with the 'main' function, but i added a couple of prints just for beautification sake. After the sorting is done the array shows a space after 9 and im not worried about that incase of 'future additives'. Also added big gaps between the testing and actual funciton for beautifiction sake again.

--- sorter.c
+++ sorter.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 const int MAX = 9;
+
 //Function to print the values of an integer array
 void printValues(int* numArr) {
   
@@ -7,7 +8,7 @@ void printValues(int* numArr) {
   printf("[");
   
   //Iterates through the array and prints each value
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < MAX; i++) {
     printf("%d, ", numArr[i]);
   };
 
@@ -16,38 +17,60 @@ void printValues(int* numArr) {
 };
 
 //Function to sort the array
-void sort(int* values) {
+void sort(int* numArr) {
 
-  //checks each value against the next value
-  for (int c1 = 0, c2 = 1; c1 < 9; c1++, c2++) {
-    if (values[c1] > values[c2]) {
-      printf("%d, %d \n", values[c1], values[c2]);
-    //  swap(&values[c1], &values[c2]);
-    else {
-      printf("No swap needed: %d, %d \n", values[c1], values[c2]);
-    }
+  //Loops the sorting process until the array is fully sorted, checks the first and last number in the array to see if they are 1 and 9
+  while (numArr[8] != MAX || numArr[0] != 1) {
+
+    //Iterates through the array, comparing each number to the next one
+    for (int c1 = 0, c2 = 1; c1 < MAX; c1++, c2++) {
+
+      //If the first number is larger than the second, swap them
+      if (numArr[c1] > numArr[c2]) {
+        //Testing feature to see what is being  selected
+        //printf("%d, %d \n", numArr[c1], numArr [c2]);
+
+        //Calls the swap function to swap the two numbers
+        swap(&numArr[c1], &numArr[c2]);
+
+        //Prints the current state of the array after each swap
+        printValues(numArr);
+  
+      };
     };
   };
+  printf("Sorting Done \n");
 };
 
-void swap(int*, int*) {
-  printf("\n");
+
+//Function to swap two integers
+void swap(int* firNum, int* secNum) {
+
+  //Testing feature to see what is being swapped
+  //printf("Swapping: %d, %d \n", *firNum, *secNum);
+  
+  //Swaps the two integers using a temporary variable
+  int temp = *firNum;
+  *firNum = *secNum;
+  *secNum = temp;
 };
+
 
 int main(){
   int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
   printf("Before: ");
   printValues(&values);
 
-  // test swap
+  //test swap
   int x = 3;
   int y = 5;
   printf("x: %d, y: %d \n", x, y);
   swap(&x, &y);
-  printf("x: %d, y: %d \n", x, y);
+  printf("x: %d, y: %d \n\n", x, y);
 
+  printf("\nNumbers sorting...\n");
   sort(&values);
-  printf("After: \n");
+  printf("\nAfter: ");
   printValues(&values);
   
   return(0);
